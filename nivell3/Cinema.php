58ac6a6fc321231477344d526078eebde283fb50
Laurea Gregori <gregoridelrio@gmail.com>
2025-12-18 12:27:40 +0100

    add: main.php

--- nivell3/Cinema.php
+++ nivell3/Cinema.php
@@ -50,28 +50,3 @@ class Cinema
     }
   }
 }
-
-$cinema1 = new Cinema("Cinema Girona", "Girona");
-$cinema2 = new Cinema("Cinema Barcelona", "Barcelona");
-
-$pelicula1 = new Pelicula("Rocky", 120, "Avildsen");
-$pelicula2 = new Pelicula("El Padrino", 200, "Coppola");
-$pelicula3 = new Pelicula("El Padrino 2", 205, "Coppola");
-$pelicula4 = new Pelicula("Oppenheimer", 210, "Nolan");
-$pelicula5 = new Pelicula("Zootropolis", 100, "Moore");
-
-$cinema1->afegirPelicula($pelicula1);
-$cinema1->afegirPelicula($pelicula2);
-$cinema1->afegirPelicula($pelicula3);
-$cinema1->afegirPelicula($pelicula4);
-
-$cinema2->afegirPelicula($pelicula2);
-$cinema2->afegirPelicula($pelicula3);
-$cinema2->afegirPelicula($pelicula4);
-$cinema2->afegirPelicula($pelicula5);
-
-$cinema1->buscarDirector("Coppola", [$cinema1, $cinema2]);
-
-//$cinema1->mostrarPelicules();
-
-//$cinema1->mostrarPeliculaMesLlarga();
---
+++ nivell3/main.php
@@ -0,0 +1,27 @@
+<?php
+require_once "Cinema.php";
+
+$cinema1 = new Cinema("Cinema Girona", "Girona");
+$cinema2 = new Cinema("Cinema Barcelona", "Barcelona");
+
+$pelicula1 = new Pelicula("Rocky", 120, "Avildsen");
+$pelicula2 = new Pelicula("El Padrino", 200, "Coppola");
+$pelicula3 = new Pelicula("El Padrino 2", 205, "Coppola");
+$pelicula4 = new Pelicula("Oppenheimer", 210, "Nolan");
+$pelicula5 = new Pelicula("Zootropolis", 100, "Moore");
+
+$cinema1->afegirPelicula($pelicula1);
+$cinema1->afegirPelicula($pelicula2);
+$cinema1->afegirPelicula($pelicula3);
+$cinema1->afegirPelicula($pelicula4);
+
+$cinema2->afegirPelicula($pelicula2);
+$cinema2->afegirPelicula($pelicula3);
+$cinema2->afegirPelicula($pelicula4);
+$cinema2->afegirPelicula($pelicula5);
+
+echo $cinema1->buscarDirector("Coppola", [$cinema1, $cinema2]) . "<br>";
+
+echo $cinema1->mostrarPelicules() . "<br>";
+
+echo $cinema1->mostrarPeliculaMesLlarga() . "<br>";
